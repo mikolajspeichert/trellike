6d30c7d4fdc5342bac4668175c25fb00fba23ee0
mikolajspeichert <mikolaj.speichert@gmail.com>
2018-11-22 11:48:42 +0100

    Updates

---
+++ precache-manifest.02c6184490c5de2023bd6df27c1f6e7c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee5f8d14a9fdb2cd3dd0",
+    "url": "/trellike/static/js/runtime~main.ee5f8d14.js"
+  },
+  {
+    "revision": "6b35daea8e0e56328181",
+    "url": "/trellike/static/js/main.6b35daea.chunk.js"
+  },
+  {
+    "revision": "d2aa65f89e660dd50c53",
+    "url": "/trellike/static/js/1.d2aa65f8.chunk.js"
+  },
+  {
+    "revision": "12576ad992eb762118d3ba517b1b0df1",
+    "url": "/trellike/index.html"
+  }
+];
